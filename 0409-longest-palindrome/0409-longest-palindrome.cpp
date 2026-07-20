bbb96598edef08800dd91b0995678e2cc03269fd
Shivam Kumar <3929sunilshivam@gmail.com>
2026-07-20 23:37:10 +0530

    Time: 1 ms (27.51%), Space: 9.3 MB (42.99%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
+        int oddCount = 0;
+        unordered_map<char, int> ump;
+        for(char ch : s) {
+            ump[ch]++;
+            if (ump[ch] % 2 == 1)
+                oddCount++;
+            else    
+                oddCount--;
+        }
+        if (oddCount > 1)
+            return s.length() - oddCount + 1;
+        return s.length();
         
-        vector<int>lower(26,0);
-        vector<int>upper(26,0);
-        for(int i=0 ; i<s.size();i++){
-            if(s[i]>='a'){
-                lower[s[i]-'a']++;
-            }
-            else{
-                upper[s[i]-'A']++;
-            }
-        }
-        int count=0;
-        bool odd=0;
-        for(int i=0;i<26;i++){
-            if(lower[i]%2==0){
-                count +=lower[i];
-            }
-            else{
-                count +=lower[i]-1;
-                odd=1;
-            }
-            if(upper[i]%2==0){
-                count += upper[i];
-
-            }
-            else{
-                count += upper[i]-1;
-                odd=1;
-            }
-        }
-        return count+odd;
     }
 };
