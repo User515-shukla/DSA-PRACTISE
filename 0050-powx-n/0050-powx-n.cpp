82ee869bc78410482007919441518bd5f7f696bb
Shivam Kumar <3929sunilshivam@gmail.com>
2026-08-28 00:14:38 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.55%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        if(n==0)
+        return 1;
+        double halfpower=myPow(x,n/2);
+        double halfpowersquare=halfpower*halfpower;
+        if(n%2==0){
+            return halfpowersquare;
+        }
+        else{
+            if(n<0){
+                return (1/x)* halfpowersquare;
+            }
+            else{
+               return x*halfpowersquare;
+            }
+        }
         
     }
 };
